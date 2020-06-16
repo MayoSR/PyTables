0a170daa1e4e1e1eea409681aed02656fcd7908d
Mayankrao-16 <55399795+Mayankrao-16@users.noreply.github.com>
2020-06-16 22:08:36 +0530

    Added OOPS

--- Table.py
+++ Table.py
@@ -1,67 +1,134 @@
 import colorama
 from colorama import Fore, Style, Back
+import random
+
+color_map = {
+    "WHITE": Fore.WHITE,
+    "BLUE": Fore.BLUE,
+    "GREEN": Fore.GREEN,
+    "BLACK": Fore.BLACK,
+    "RED": Fore.RED,
+    "YELLOW": Fore.YELLOW,
+    "CYAN": Fore.CYAN,
+    "MAGENTA": Fore.MAGENTA,
+    "LIGHTBLACK": Fore.LIGHTBLACK_EX,
+    "LIGHTBLUE": Fore.LIGHTBLUE_EX,
+    "LIGHTCYAN": Fore.LIGHTCYAN_EX,
+    "LIGHTGREEN": Fore.LIGHTGREEN_EX,
+    "LIGHTMAGENTA": Fore.LIGHTMAGENTA_EX,
+    "LIGHTRED": Fore.LIGHTRED_EX,
+    "LIGHTWHITE": Fore.LIGHTWHITE_EX,
+    "LIGHTYELLOW": Fore.LIGHTYELLOW_EX,
+    "reset": Fore.RESET
+}
+
+brightness_map = {
+    "BRIGHT": Style.BRIGHT,
+    "NORMAL": Style.NORMAL,
+    "DIM": Style.DIM
+}
+
+background_map = {
+    "WHITE": Back.WHITE,
+    "BLUE": Back.BLUE,
+    "GREEN": Back.GREEN,
+    "BLACK": Back.BLACK,
+    "RED": Back.RED,
+    "YELLOW": Back.YELLOW,
+    "CYAN": Back.CYAN,
+    "MAGENTA": Back.MAGENTA,
+    "LIGHTBLACK": Back.LIGHTBLACK_EX,
+    "LIGHTBLUE": Back.LIGHTBLUE_EX,
+    "LIGHTCYAN": Back.LIGHTCYAN_EX,
+    "LIGHTGREEN": Back.LIGHTGREEN_EX,
+    "LIGHTMAGENTA": Back.LIGHTMAGENTA_EX,
+    "LIGHTRED": Back.LIGHTRED_EX,
+    "LIGHTWHITE": Back.LIGHTWHITE_EX,
+    "LIGHTYELLOW": Back.LIGHTYELLOW_EX,
+    "reset": Back.RESET
+}
+
+default_color_set = Fore.WHITE
+
 
 class Table(object):
 
-    color_map = {"WHITE": Fore.WHITE, "BLUE": Fore.BLUE, "GREEN": Fore.GREEN,
-                 "BLACK": Fore.BLACK, "RED": Fore.RED, "YELLOW": Fore.YELLOW, "CYAN": Fore.CYAN, "MAGENTA": Fore.MAGENTA, "LIGHTBLACK": Fore.LIGHTBLACK_EX, "LIGHTBLUE": Fore.LIGHTBLUE_EX, "LIGHTCYAN": Fore.LIGHTCYAN_EX, "LIGHTGREEN": Fore.LIGHTGREEN_EX, "LIGHTMAGENTA": Fore.LIGHTMAGENTA_EX, "LIGHTRED": Fore.LIGHTRED_EX, "LIGHTWHITE": Fore.LIGHTWHITE_EX, "LIGHTYELLOW": Fore.LIGHTYELLOW_EX, "reset": Fore.RESET}
-
     def __init__(self, headers):
         self.max_cell_size_by_column = [0 for i in range(len(headers))]
-        self.table = [headers]
+        self.table_len = len(headers)
+        self.table = []
+        self.add_row(headers, header=True)
         self.body_color = "WHITE"
         self.header_color = "WHITE"
         self.border_color = "WHITE"
         self.background_color = None
 
-    def add_row(self, row):
-        self.table.append(row)
+    def add_row(self, row, header=False):
+
+        self.table.append([TableCell(header, i, random.choice(list(color_map.keys())), random.choice(
+            list(background_map.keys())), random.choice(list(brightness_map.keys()))) for i in row])
 
     def check_max_cell_size_by_column(self):
 
         for i in self.table:
             col_ind = 0
             for j in i:
-                self.max_cell_size_by_column[col_ind] = len(j) if len(
-                    j) > self.max_cell_size_by_column[col_ind] else self.max_cell_size_by_column[col_ind]
+                self.max_cell_size_by_column[col_ind] = j.get_text_length() if j.get_text_length(
+                ) > self.max_cell_size_by_column[col_ind] else self.max_cell_size_by_column[col_ind]
                 col_ind += 1
-
-    def print_row(self, text, header=False):
-        if header:
-            print("| "+(Table.color_map[self.header_color] + str(text[1]) + Table.color_map["reset"])+" "*(self.max_cell_size_by_column[text[0]] - len(str(text[1])) + 1), end="")
-        else:
-            print("| "+(Table.color_map[self.body_color] + str(text[1]) + Table.color_map["reset"])+" "*(self.max_cell_size_by_column[text[0]] - len(str(text[1])) + 1), end="")
-        
 
     def display(self):
         self.check_max_cell_size_by_column()
-        print(Table.color_map[self.border_color]+"+"+(sum([i+2 for i in self.max_cell_size_by_column]
-                                                          )+len(self.table[0]) - 1)*"-"+"+"+Table.color_map["reset"])
+        print(color_map[self.border_color]+"+"+(sum([i+2 for i in self.max_cell_size_by_column]) +
+                                                self.table_len - 1)*"-"+"+"+color_map["reset"])
         for i in enumerate(self.table):
             for j in enumerate(i[1]):
-                if i[0] == 0:
-                    self.print_row(j, True)
-                else:
-                    self.print_row(j)
-            print("|"+Table.color_map["reset"])
-            print(Table.color_map[self.border_color]+"+"+(sum([i+2 for i in self.max_cell_size_by_column]
-                                                              )+len(self.table[0]) - 1)*"-"+"+"+Table.color_map["reset"])
+                print("| "+j[1].get_text()+" " *
+                      (self.max_cell_size_by_column[j[0]] - j[1].get_text_length() + 1), end="")
+            print("|")
+            print(color_map[self.border_color]+"+"+(sum([i+2 for i in self.max_cell_size_by_column]
+                                                        )+self.table_len - 1)*"-"+"+"+color_map["reset"])
 
-    def coloumn_headers(self,headers):
-        pass
-    
-    def styles(self, header_color="WHITE", body_color="WHITE", border_color="WHITE", background_color=None):
-        self.body_color = body_color.upper()
-        self.border_color = border_color.upper()
-        self.background_color = background_color
-        self.header_color = header_color.upper()
 
 class TableCell(object):
-    pass
+
+    def __init__(self, header, text, color=default_color_set, bg=default_color_set, style=default_color_set):
+        self.text = text
+        self.color = color
+        self.bg = bg
+        self.style = style
+        self.header = header
+        self.set_styles_to_text()
+
+    def get_text_length(self):
+        return len(self.text)
+
+    def get_text(self):
+        return str(self.text)
+
+    def set_styles_to_text(self):
+        self.text = self.color + self.bg + \
+            self.style + self.text + color_map["reset"]
+
+    def set_text(self, text):
+        self.text = text
+        self.set_styles_to_text()
+
+    def set_color(self, color):
+        self.color = color
+        self.set_styles_to_text()
+
+    def set_bg(self, bg):
+        self.bg = bg
+        self.set_styles_to_text()
+
+    def set_style(self, style):
+        self.style = style
+        self.set_styles_to_text()
+
 
 table = Table(["hello", "world", "what", "is", "happening"])
 table.add_row(["23", "43", "55", "112", "42545"])
 table.add_row(["22333", "3445", "None", "23434", "545555"])
 table.add_row(["2352455", "79797", "9998", "2352455", "213422"])
-table.styles(header_color="green", body_color="RED")
 table.display()
